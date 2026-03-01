35f278192b7c2c7fa29d9e319f99f5b5fc7c1231
RosieOh <dhxogns920@gmail.com>
2026-03-02 00:13:39 +0900

    FEAT : 비밀번호 찾기 요청 DTO 추가 (ForgotPasswordRequest) - 아이디, 이메일로 비밀번호 재설정 링크 발송 요청 데이터 정의

---
+++ src/main/java/com/cornedu/api/member/dto/ForgotPasswordRequest.java
@@ -0,0 +1,12 @@
+package com.cornedu.api.member.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ForgotPasswordRequest {
+    @NotBlank
+    private String email;
+}
