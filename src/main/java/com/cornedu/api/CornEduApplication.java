412e6816b74befd6f250a67fc7b48ad2474b1405
RosieOh <dhxogns920@gmail.com>
2026-03-02 00:11:12 +0900

    FEAT : Spring Boot 애플리케이션 메인 클래스 추가 (CornEduApplication) - corn-edu-api 프로젝트 시작점 설정

---
+++ src/main/java/com/cornedu/api/CornEduApplication.java
@@ -0,0 +1,11 @@
+package com.cornedu.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CornEduApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CornEduApplication.class, args);
+    }
+}
